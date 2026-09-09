47b4a2b5473be9b0418ef6266a736a9b997809f6
buymyhubs <dairyisfine@gmail.com>
2026-09-09 13:38:46 -0500

    Enable inline remote flag on headphone state update

--- src/hardware/headphone_status.c
+++ src/hardware/headphone_status.c
@@ -9,6 +9,8 @@
 #define HEADSET_SWITCH_STATE_PATH "/sys/devices/virtual/switch/headset/state"
 #define BALANCED_SWITCH_STATE_PATH "/sys/devices/virtual/switch/balance/state"
 
+// must be written "on" before headphone buttons can trigger input events
+#define EARPODS_ADC_SW_PATH "/sys/devices/platform/earpods_adc/earpods_adc/earpods_adc_sw"
 
 // returns true if headset 3.5mm jack is plugged in
 bool headset_is_connected(void) {
@@ -34,14 +36,33 @@ bool balanced_is_connected(void) {
     return ok && buf[0] == '1';
 }
 
+// enable button inputs for headsets with inline remote
+static void set_earpods_adc_enabled(bool enabled) {
+    FILE * f = fopen(EARPODS_ADC_SW_PATH, "w");
+    if (!f) return;
+    fputs(enabled ? "on" : "off", f);
+    fclose(f);
+}
+
 // returns which headphone output is plugged in
 // if 3.5mm and 4.4mm are both plugged in, 4.4mm is prioritized
+// syncs earpods_adc_sw with headphone state
 enum HEADPHONE_STATE get_headphone_state(void) {
-	if (balanced_is_connected()) {
-		return HEADPHONE_STATE_BALANCED;
-	} else if (headset_is_connected()) {
-		return HEADPHONE_STATE_HEADSET;
-	}
+    static enum HEADPHONE_STATE last_state = HEADPHONE_STATE_NONE;
+    enum HEADPHONE_STATE state;
 
-	return HEADPHONE_STATE_NONE;
+    if (balanced_is_connected()) {
+        state = HEADPHONE_STATE_BALANCED;
+    } else if (headset_is_connected()) {
+        state = HEADPHONE_STATE_HEADSET;
+    } else {
+        state = HEADPHONE_STATE_NONE;
+    }
+
+    if (state != last_state) {
+        set_earpods_adc_enabled(state == HEADPHONE_STATE_HEADSET);
+        last_state = state;
+    }
+
+    return state;
 }
